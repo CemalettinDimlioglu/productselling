7866b0f9cdb66453517f3f5de8b75dfdc579c7f9
CMLTTN <cemaldim@gmail.com>
2023-07-06 14:40:04 +0300

    Not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>404 Error</h1>
+        </div>
+      </div>
+      <div className="container content" >
+          <p>Page Not Found...</p>
+          <Link to="/"className="btn btn-primary">Go Back to Home Page</Link>
+      </div>
+    </main>
+  );
+};
 
-export default NotFound
+export default NotFound;
