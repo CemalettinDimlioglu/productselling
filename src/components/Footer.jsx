fe80e22054a1e6a6e58a3c9a3bad9fab611175a7
CMLTTN <cemaldim@gmail.com>
2023-07-06 13:59:21 +0300

    footer+html

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="container">
+        <nav className="d-flex justify-content-center">
+          <NavLink
+            to="/"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Products
+          </NavLink>
+          
+        </nav>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
