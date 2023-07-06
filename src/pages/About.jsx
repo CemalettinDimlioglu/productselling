bc8f37c68770f51d7e6c0bc750491bb6cd7b6c32
CMLTTN <cemaldim@gmail.com>
2023-07-06 13:39:08 +0300

    about +home

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <main className='about' >
+    <div className='pg-header' >
+      <div className='container' >
+        <h1>
+          About
+        </h1>
+      </div>
+    </div>
+    <div className='container content' > 
+      <div className='row' >
+        <div className='col-lg-4' >
+          <h2>Why Choose Us...</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quod?</p>
+        </div>
+        <div className='col-lg-4' >
+          <h2>What We Do?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, nesciunt.</p>
+        </div>
+      </div>
+    </div>
+    </main>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <div className='pg-header' >
+        <div className='container' >
+          <h1>Welcome to webShop</h1>
+        </div>
+      </div>
+      <div className='container content' >
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos non, obcaecati id enim praesentium ad architecto, fugit veniam beatae dolor vitae ex ipsum assumenda dolore illo temporibus dolorum adipisci quibusdam!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quia, harum natus dolorem quam totam rem atque perspiciatis minus ullam? Enim officia facilis temporibus facere at numquam commodi ducimus, velit cumque neque in voluptatum asperiores nostrum perferendis explicabo fugiat culpa.</p>
+      </div>
+    </main>
   )
 }
 
